19a52acfec4e9ec171353db589a9e224f293b208
majuhdl <34109238+majuhdl@users.noreply.github.com>
2022-07-28 18:19:26 -0300

    Adiciona itens iniciais

--- carrinhoCompras/src/carrinhoCompras/Cliente.java
+++ carrinhoCompras/src/carrinhoCompras/Cliente.java
@@ -1,6 +1,30 @@
 package carrinhoCompras;
 
 public abstract class Cliente {
-	String nome;
+
 	CarrinhoCompras carrinho;
+	private String Nome;
+    private String numeroConta;
+    
+	public Cliente(String nome, String numeroConta) {
+        Nome = nome;
+        this.numeroConta = numeroConta;
+    }
+    public String getNome() {
+        return Nome;
+    }
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+    @Override
+    public String toString() {
+        return "Pessoa [Nome=" + Nome + ", numeroConta=" + numeroConta + "]";
+    }
+    
 }
