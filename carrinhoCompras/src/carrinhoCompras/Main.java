bb9ea8b016d8f988f42f96dae1be9f76f3c02695
majuhdl <34109238+majuhdl@users.noreply.github.com>
2022-07-28 18:20:49 -0300

    Adiciona itens iniciais

--- carrinhoCompras/src/carrinhoCompras/Main.java
+++ carrinhoCompras/src/carrinhoCompras/Main.java
@@ -1,1 +1,30 @@
 
+package carrinhoCompras;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Item itemTeste1 = new Item("Maçã", 5, 0.50);
+        Item itemTeste2 = new Item("Banana", 5, 0.30);
+        Item itemTeste3 = new Item("Morango", 5, 0.50);
+        Item itemTeste4 = new Item("Morango", 5, 0.50);
+
+        CarrinhoCompras carrinho = new CarrinhoCompras();
+
+        carrinho.adicionaItem(itemTeste1);
+        carrinho.adicionaItem(itemTeste2);
+        carrinho.adicionaItem(itemTeste3);
+        carrinho.adicionaItem(itemTeste4);
+
+        System.out.println("Carrinho pos-adicao:");
+        carrinho.listaCarrinho();
+
+        carrinho.removeItem(itemTeste2);
+
+        System.out.println("Carrinho pos-exclusao:");
+        carrinho.listaCarrinho();
+
+    }
+    
+}
