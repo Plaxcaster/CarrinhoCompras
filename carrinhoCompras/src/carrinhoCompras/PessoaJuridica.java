84dc3be3ce2b4cbc340e58efb687307907a423d8
majuhdl <34109238+majuhdl@users.noreply.github.com>
2022-07-28 18:18:00 -0300

    Adiciona uma versão inicial

--- carrinhoCompras/src/carrinhoCompras/PessoaJuridica.java
+++ carrinhoCompras/src/carrinhoCompras/PessoaJuridica.java
@@ -2,4 +2,21 @@ package carrinhoCompras;
 
 public class PessoaJuridica extends Cliente{
 
+    private String Cnpj;
+
+    public PessoaJuridica(String nome, String numeroConta, String Cnpj) {
+        super(nome, numeroConta);
+        this.Cnpj = Cnpj;
+        //TODO Auto-generated constructor stub
+    }
+
+    public String getCnpj() {
+        return Cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.Cnpj = cnpj;
+    }
+    
+
 }
