3c6773afebee7d5691e01539d9f344d15e8e2c63
majuhdl <34109238+majuhdl@users.noreply.github.com>
2022-07-28 18:18:32 -0300

    Adiciona itens iniciais

--- carrinhoCompras/src/carrinhoCompras/PessoaFisica.java
+++ carrinhoCompras/src/carrinhoCompras/PessoaFisica.java
@@ -2,4 +2,21 @@ package carrinhoCompras;
 
 public class PessoaFisica extends Cliente{
 
+    private String Cpf;
+
+    public PessoaFisica(String nome, String numeroConta, String Cpf) {
+        super(nome, numeroConta);
+        this.Cpf = Cpf;
+        //TODO Auto-generated constructor stub
+    }
+
+    public String getCpf() {
+        return Cpf;
+    }
+
+    public void setCpf(String cpf) {
+        Cpf = cpf;
+    }
+    
+
 }
