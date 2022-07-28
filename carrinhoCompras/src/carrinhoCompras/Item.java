1851a55f543dee5232cba80fa98af2caba52a282
majuhdl <34109238+majuhdl@users.noreply.github.com>
2022-07-28 18:19:00 -0300

    Adicionar itens iniciais

--- carrinhoCompras/src/carrinhoCompras/Item.java
+++ carrinhoCompras/src/carrinhoCompras/Item.java
@@ -1,6 +1,25 @@
 package carrinhoCompras;
 
 public class Item {
-	String nome;
-	double precoUnitario;
+
+    protected String Produto;
+    protected int Quantidade;
+    double PrecoUnitario;
+    double TotalPorItem;
+
+    public Item(String produto, int quantidade, double precoUnitario) {
+        this.Produto = produto;
+        this.Quantidade = quantidade;
+        this.PrecoUnitario = precoUnitario;
+        this.TotalPorItem = Quantidade*PrecoUnitario;
+    }
+
+	@Override
+	public String toString() {
+		return "Item [PrecoUnitario=" + PrecoUnitario + ", Produto=" + Produto + ", Quantidade=" + Quantidade
+				+ ", TotalPorItem=" + TotalPorItem + "]";
+	}
+
+	
+
 }
