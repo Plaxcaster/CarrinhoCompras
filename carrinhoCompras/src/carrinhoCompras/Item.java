556a76ac19760ab7411bbe8963cd3a45103a20cc
F4955549 Joao Paulo Cunha Botelho <cunha.botelho@bb.com.br>
2022-07-29 15:51:14 -0300

    enum tipoProduto com as categorias e os descontos

--- carrinhoCompras/src/carrinhoCompras/Item.java
+++ carrinhoCompras/src/carrinhoCompras/Item.java
@@ -3,23 +3,70 @@ package carrinhoCompras;
 public class Item {
 
     protected String Produto;
+    private TipoProduto tipoProduto;
     protected int Quantidade;
     double PrecoUnitario;
     double TotalPorItem;
 
-    public Item(String produto, int quantidade, double precoUnitario) {
+
+    public Item(String produto, TipoProduto tipoProduto, int quantidade, double precoUnitario) {
         this.Produto = produto;
+        this.tipoProduto = tipoProduto;
         this.Quantidade = quantidade;
         this.PrecoUnitario = precoUnitario;
         this.TotalPorItem = Quantidade*PrecoUnitario;
     }
 
+    //tostring
 	@Override
 	public String toString() {
 		return "Item [PrecoUnitario=" + PrecoUnitario + ", Produto=" + Produto + ", Quantidade=" + Quantidade
 				+ ", TotalPorItem=" + TotalPorItem + "]";
 	}
 
+	
+	//Getters e Setters
+	public String getProduto() {
+		return Produto;
+	}
+
+	public void setProduto(String produto) {
+		Produto = produto;
+	}
+
+	public TipoProduto getTipoProduto() {
+		return tipoProduto;
+	}
+
+	public void setTipoProduto(TipoProduto tipoProduto) {
+		this.tipoProduto = tipoProduto;
+	}
+
+	public int getQuantidade() {
+		return Quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		Quantidade = quantidade;
+	}
+
+	public double getPrecoUnitario() {
+		return PrecoUnitario;
+	}
+
+	public void setPrecoUnitario(double precoUnitario) {
+		PrecoUnitario = precoUnitario;
+	}
+
+	public double getTotalPorItem() {
+		return TotalPorItem;
+	}
+
+	public void setTotalPorItem(double totalPorItem) {
+		TotalPorItem = totalPorItem;
+	}
+
+	
 	
 
 }
--- carrinhoCompras/src/carrinhoCompras/Main.java
+++ carrinhoCompras/src/carrinhoCompras/Main.java
@@ -5,10 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
         
-        Item itemTeste1 = new Item("Maçã", 5, 0.50);
-        Item itemTeste2 = new Item("Banana", 5, 0.30);
-        Item itemTeste3 = new Item("Morango", 5, 0.50);
-        Item itemTeste4 = new Item("Morango", 5, 0.50);
+        Item itemTeste1 = new Item("Maçã", TipoProduto.Hortifruti, 5, 0.50);
+        Item itemTeste2 = new Item("Banana", TipoProduto.Hortifruti, 5, 0.30);
+        Item itemTeste3 = new Item("Morango", TipoProduto.Hortifruti, 5, 0.50);
+        Item itemTeste4 = new Item("Morango", TipoProduto.Hortifruti, 5, 0.50);
 
         CarrinhoCompras carrinho = new CarrinhoCompras();
 
@@ -16,12 +16,14 @@ public class Main {
         carrinho.adicionaItem(itemTeste2);
         carrinho.adicionaItem(itemTeste3);
         carrinho.adicionaItem(itemTeste4);
-
+        
+        System.out.println();
         System.out.println("Carrinho pos-adicao:");
         carrinho.listaCarrinho();
 
         carrinho.removeItem(itemTeste2);
 
+        System.out.println();
         System.out.println("Carrinho pos-exclusao:");
         carrinho.listaCarrinho();
 
