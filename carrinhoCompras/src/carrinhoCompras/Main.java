f6bf64956aa34a5c64fd94b0a9ad9d90b3906557
majuhdl <34109238+majuhdl@users.noreply.github.com>
2022-08-03 15:27:58 -0300

    Atualiza a main

--- carrinhoCompras/src/carrinhoCompras/Main.java
+++ carrinhoCompras/src/carrinhoCompras/Main.java
@@ -1,39 +1,66 @@
 
 package carrinhoCompras;
 
+import carrinhoCompras.TipoProduto.TipoProd;
+
 public class Main {
 
     public static void main(String[] args) {
         
-        Item itemTeste1 = new Item("Maca", TipoProduto.Hortifruti, 5, 0.50);
-        Item itemTeste2 = new Item("Banana", TipoProduto.Hortifruti, 5, 0.30);
-        Item itemTeste3 = new Item("Morango", TipoProduto.Hortifruti, 5, 1.00);
-        Item itemTeste4 = new Item("Morango", TipoProduto.Hortifruti, 5, 1.00);
+        Item itemTeste1 = new Item("Maca", TipoProduto.TipoProd.Hortifruti, 5, 0.50);
+        Item itemTeste2 = new Item("Banana", TipoProduto.TipoProd.Hortifruti, 5, 0.30);
+        Item itemTeste3 = new Item("Morango", TipoProduto.TipoProd.Hortifruti, 5, 1.00);
+        Item itemTeste4 = new Item("Morango", TipoProduto.TipoProd.Hortifruti, 5, 1.00);
+        Item itemTeste5 = new Item("Picanha", TipoProduto.TipoProd.Carnes, 1, 100.00);
 
         CarrinhoCompras carrinho = new CarrinhoCompras();
 
-        carrinho.adicionaItem(itemTeste1);
-        carrinho.adicionaItem(itemTeste2);
-        carrinho.adicionaItem(itemTeste3);
-        carrinho.adicionaItem(itemTeste4);
+        AddItem.adicionaItem(carrinho, itemTeste1);
+        AddItem.adicionaItem(carrinho, itemTeste2);
+        AddItem.adicionaItem(carrinho, itemTeste3);
+        AddItem.adicionaItem(carrinho, itemTeste4);
+        AddItem.adicionaItem(carrinho, itemTeste5);
         
         System.out.println();
         System.out.println("Carrinho pos-adicao:");
-        carrinho.listaCarrinho();
-        carrinho.calcularTotal();
+        ImprimeCarrinho.listaCarrinho(carrinho);
+        TotalCarrinho.calcularTotal(carrinho);
         
         System.out.println();
         System.out.println("Carrinho pos-ordenacao:");
-        carrinho.ordenaCarrinho(TipoOrdenacao.QUANTIDADE);
-        carrinho.listaCarrinho();
-        carrinho.calcularTotal();
+        Ordenar.ordenaCarrinho(carrinho, TipoOrdenacao.QUANTIDADE);
+        ImprimeCarrinho.listaCarrinho(carrinho);
+        TotalCarrinho.calcularTotal(carrinho);
         
-        carrinho.removeItem(itemTeste2);
+        RemoveItem.removeItem(carrinho, itemTeste2);
         
         System.out.println();
         System.out.println("Carrinho pos-exclusao:");
-        carrinho.listaCarrinho();
-        carrinho.calcularTotal();
+        ImprimeCarrinho.listaCarrinho(carrinho);
+        TotalCarrinho.calcularTotal(carrinho);
+
+        System.out.println();
+        System.out.println("Item removido:");
+        AlteraQtdItem.alterarQtdItem(carrinho, carrinho.carrinho.get(1), 2,"Remover");
+        ImprimeCarrinho.listaCarrinho(carrinho);
+
+        System.out.println();
+        System.out.println("Item adicionado:");
+        AlteraQtdItem.alterarQtdItem(carrinho, carrinho.carrinho.get(1), 20,"Adicionar");
+        ImprimeCarrinho.listaCarrinho(carrinho);
+
+        System.out.println();
+        System.out.println("Taxa extra no item hortifruti:");
+        TipoProduto.TipoProd hortifruti = TipoProd.Hortifruti;
+        TaxaExtra.taxaExtra(carrinho, hortifruti, 0.15);
+        ImprimeCarrinho.listaCarrinho(carrinho);
+
+        System.out.println();
+        TipoProduto.TipoProd carne = TipoProd.Carnes;
+        FretePct.freteItem(carrinho, carne, 0.50);
+        ImprimeCarrinho.listaCarrinho(carrinho);
+
     }
     
 }
+
