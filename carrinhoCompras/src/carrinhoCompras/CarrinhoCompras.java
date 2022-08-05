acac766908217a3a32e74870ebdaf483ed6ff985
majuhdl <34109238+majuhdl@users.noreply.github.com>
2022-08-05 15:37:12 -0300

    Adiciona cliente no carrinho

--- carrinhoCompras/src/carrinhoCompras/CarrinhoCompras.java
+++ carrinhoCompras/src/carrinhoCompras/CarrinhoCompras.java
@@ -5,7 +5,28 @@ import java.util.List;
 
 public class CarrinhoCompras {
 
-    List<Item> carrinho = new ArrayList<>();
+    public Cliente cliente;
+    public List<Item> carrinho = new ArrayList<>();
 
+    public void carrinhoCompras(Cliente cliente, List<Item> carrinho){
+        this.cliente = cliente;
+        this.carrinho = carrinho;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Item> getCarrinho() {
+        return carrinho;
+    }
+
+    public void setCarrinho(List<Item> carrinho) {
+        this.carrinho = carrinho;
+    }
  	
 }
