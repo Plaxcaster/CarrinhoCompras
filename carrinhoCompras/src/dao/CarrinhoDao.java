c65cd0361ca9b26e4199fd5807f7e27790d60bbf
majuhdl <34109238+majuhdl@users.noreply.github.com>
2022-08-05 15:30:13 -0300

    Altera carrinhodao

--- carrinhoCompras/src/dao/CarrinhoDao.java
+++ carrinhoCompras/src/dao/CarrinhoDao.java
@@ -7,36 +7,36 @@ import java.util.TreeSet;
 
 public class CarrinhoDao<CarrinhoCompras> implements BaseDao<CarrinhoCompras, String> {
 
-    private List<CarrinhoCompras> veiculos = new ArrayList<>();
+    private List<CarrinhoCompras> carrinhos = new ArrayList<>();
 
     @Override
-    public CarrinhoCompras salvar(CarrinhoCompras veiculo) {
-        veiculos.add(veiculo);
-        System.out.println("veiculo salvo" + veiculo);
-        return veiculo;
+    public CarrinhoCompras salvar(CarrinhoCompras carrinho) {
+        carrinhos.add(carrinho);
+        System.out.println("Carrinho salvo \n" + carrinho);
+        return carrinho;
     }
 
     @Override
     public List<CarrinhoCompras> buscarTodos() {
-        return veiculos;
+        return carrinhos;
     }
 
     @Override
     public TreeSet<CarrinhoCompras> buscarTodos(Comparator<CarrinhoCompras> comparator) {
         final TreeSet<CarrinhoCompras> veiculosComparados = new TreeSet<CarrinhoCompras>(comparator);
-        veiculosComparados.addAll(veiculos);
+        veiculosComparados.addAll(carrinhos);
         return veiculosComparados;
     }
 
     @Override
-    public void remover(CarrinhoCompras veiculo) {
-        veiculos.remove(veiculo);
-        System.out.println("veiculo removido" + veiculo);
+    public void remover(CarrinhoCompras carrinho) {
+        carrinhos.remove(carrinho);
+        System.out.println("carrinho removido" + carrinho);
     }
 
     @Override
     public CarrinhoCompras buscaPorId(int id) {
-        return veiculos.get(id);
+        return carrinhos.get(id);
     }
     
 }
